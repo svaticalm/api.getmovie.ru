bd2f8ba7e48e65c61e402f62ad55aacc103f3f52
kramskovdaniil <kramskov.daniil@yandex.ru>
2020-06-14 17:10:38 +0500

    bd

--- apigetmovie/admin.py
+++ apigetmovie/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import FavMovie, UserFavID
 
 # Register your models here.
+
+admin.site.register(FavMovie)
+admin.site.register(UserFavID)
--- apigetmovie/models.py
+++ apigetmovie/models.py
@@ -1,2 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class FavMovie(models.Model):
+    """
+    Model of favorite film users
+    """
+    id = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class UserFavID(models.Model):
+    fav_id = models.ManyToManyField(FavMovie)
+    username = models.CharField(max_length=200)
+
+    def __str__(self):
+        return " %s" % self.username
